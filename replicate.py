a0d629ec06ad4a9f258761114037bb57536fa9b4
conanhjj <conanhjj@gmail.com>
2013-05-03 19:16:12 -0500

    Fix bug.

--- replicate.py
+++ replicate.py
@@ -5,6 +5,6 @@ n1 = int(raw_input())
 n2 = int(raw_input())
 
 for i in range(n1, n2):
-        str = "receipt-replicate-%d" % i
-            os.system("hadoop dfs -copyFromLocal " + "receipt-joined.txt " + " ~/cs525/input/" + str)
-                print "copy file:", str
+    str = "receipt-replicate-%d" % i
+    os.system("hadoop dfs -copyFromLocal " + "receipt-joined.txt " + " ~/cs525/input/" + str)
+    print "copy file:", str
