f608276410937513168a631d7e10895897097fd5
conanhjj <conanhjj@gmail.com>
2013-05-03 19:08:14 -0500

    Add replicate script.

---
+++ replicate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+
+n1 = int(raw_input())
+n2 = int(raw_input())
+
+for i in range(n1, n2):
+        str = "receipt-replicate-%d" % i
+            os.system("hadoop dfs -copyFromLocal " + "receipt-joined.txt " + " ~/cs525/input/" + str)
+                print "copy file:", str
